a42b653df7139f3765e3d74ba26f4556b67decba
jordanritzie19 <jordanritzie@yahoo.com>
2022-12-06 03:21:54 -0500

    created Basic_JavaSciprt_Project folder

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JavaScript.js
@@ -0,0 +1,14 @@
+// this is how you comment in JavaScript
+
+var warning = "Tread carefully, potentially sensitive content ahead";
+var team_name = "Cincinnati Bengals"
+var position_group = "receivers ", wr1= "Jamaar Chase", wr2= "Tee Higgins", wr3 = "Tyler Boyd"; //defining variables
+            // surely this is an acceptable replacement for Sent1 and Sent2 in the requirements
+
+
+window.alert(warning)                           //this gives the pop up advising about sensitive content
+
+document.write("The " + team_name + " possess the best group of " + position_group + "in the NFL with the trio of "
++ wr1 +', ' + wr2 + ' and ' + wr3)              //writing a statement concatenating strings + variables
+
+
