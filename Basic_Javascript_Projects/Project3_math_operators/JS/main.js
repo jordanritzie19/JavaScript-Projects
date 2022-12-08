a70a83c3c802d26345eb907b32e399aa28d55a72
jordanritzie19 <jordanritzie@yahoo.com>
2022-12-08 02:41:10 -0500

    math operations

---
+++ Basic_Javascript_Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,47 @@
+function subtract() {  //subtracts 5 and 2
+    var subtraction = 5 - 2;
+    document.getElementById("subtract").innerHTML = "5 - 2 = " + subtraction;
+}
+
+function multiply() {  //multiplies 6 and 8
+    var multiplication = 6 * 8;
+    document.getElementById("multiply").innerHTML = "6 x 8 = " + multiplication;
+}
+
+function divide() {  //divides 48 by 6
+    var division = 48 / 6;
+    document.getElementById("divide").innerHTML = "48 / 6 = " + division;
+}
+
+function arithmetic() { // 1/2 * (3+6)
+    var math = .5*(3+6);
+    document.getElementById("arithmetic").innerHTML = "1/2 * (3 + 6) = " + math;
+}
+
+function remainder() { // 7 % 2
+    var modulation = 7 % 2;
+    document.getElementById("remainder").innerHTML = "The remainder after 7 over 2 = " + modulation;
+}
+
+function negate() {
+    var negation = 16;
+    document.getElementById("negate").innerHTML = -negation;
+}
+
+function increment() {
+    var increment = 5;
+    increment++;
+    document.getElementById("increment").innerHTML = increment;
+}
+
+
+function decrement() {
+    var decrement = 5;
+    decrement--;
+    document.getElementById("decrement").innerHTML = decrement;
+}
+
+function randint() {
+    var randint = (Math.random() * 10);
+    document.getElementById("randint").innerHTML = Math.round(randint);
+}
