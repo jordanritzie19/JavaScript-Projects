bdb0fbd0c83cf29f3f7835b4daffa5c0479132c0
jordanritzie19 <jordanritzie@yahoo.com>
2023-01-06 02:22:43 -0500

    Updated Pizza
    Instead of only getting a free topping if it is meat, any topping will suffice

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -37,6 +37,7 @@ function getTopping(runningTotal, text1) {
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
+    var free_topping = null;
     //this for loop parses through the list of toppings, and adds each one to the running list of selected options
     for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
@@ -47,21 +48,25 @@ function getTopping(runningTotal, text1) {
     }
     //subtracts one of the toppings as the first one is free
     var toppingCount = selectedTopping.length;
-    if (toppingCount > 1) {
+    if (toppingCount > 0) {
+        free_topping = false;  //since a topping has been selected, there is no longer a free one remaining
         toppingTotal = (toppingCount - 1);
     } else {
+        free_topping = true;   //no topping has been selected, so a free one still remains
         toppingTotal = 0;  //this is to cover the case where no toppings are selected, as we don't want to reduce the price
     }
+    
     runningTotal = (runningTotal + toppingTotal); //add topping total to the running total to be passed on
     console.log("total selected topping items: " + toppingCount);
     console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
     console.log("topping text1: " + text1);
     console.log("Purchase Total so far: "+"$" + runningTotal + ".00");
-    getVeggies(runningTotal, text1) //sends running variables over to getVeggies()
+    console.log("You would get a free veggie: " + free_topping)
+    getVeggies(runningTotal, text1, free_topping) //sends running variables over to getVeggies()
 };
 
 //this function takes the running variables and sends it through to the HTML document
-function getVeggies(runningTotal, text1) {
+function getVeggies(runningTotal, text1, free_topping) {
     //initializing veggie variables
     var veggieTotal = 0;
     var selectedVeggie = [];
@@ -75,10 +80,15 @@ function getVeggies(runningTotal, text1) {
         }
     }
     
-    var veggieTotal = selectedVeggie.length;  //each selected veggie adds a dollar to the price
+    var veggieCount = selectedVeggie.length;
+    if ((veggieCount > 0) && (free_topping == true)) { //if veggies have been selected AND there is a free topping available still:
+        veggieTotal = (veggieCount - 1);
+    } else {
+        veggieTotal = veggieCount;  //this is to cover cases where you wouldn't be granted a free topping, either due to no selection or you've already gotten one for free
+    }
     
     runningTotal = (runningTotal + veggieTotal); //adds veggie total to the running total to be sent through
-    console.log("total selected veggie items: " + veggieTotal);
+    console.log("total selected veggie items: " + veggieCount);
     console.log(veggieTotal + " veggie items = " + "$" + veggieTotal + ".00");
     console.log("veggie text1: " + text1);
     console.log("Purchase Total: "+"$" + runningTotal + ".00");
